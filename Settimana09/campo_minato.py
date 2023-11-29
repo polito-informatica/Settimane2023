140cfbd1029b448d9bf37a1e066cb2aae307f3bb
Fulvio Corno <fulvio.corno@polito.it>
2023-11-29 23:01:03 +0100

    commenti alle funzioni

--- Settimana09/campo_minato.py
+++ Settimana09/campo_minato.py
@@ -38,14 +38,14 @@ seguenti tre casi:
 
 """
 
-from operator import contains
 from random import randint
+
 
 def main():
     # leggi N e M
     # N = int(input("Dimensione del campo minato: "))
     # M = int(input("Numero di mine da inserire: "))
-    N = 10   # da togliere :)
+    N = 10  # da togliere :)
     M = 15
 
     # costruisci la matrice NxN contenente M mine
@@ -56,11 +56,22 @@ def main():
     vicine = calcola_adiacenti(mine)
     print_vicini(vicine)
 
+
 def crea_mine(N, M):
+    """Crea il campo minato sotto forma di tabella di booleani
+
+    Args:
+        N (int): lato del campo minato (quadrato di NxN celle)
+        M (int): numero di mine da inserire nel campo (deve essere << N²)
+
+    Returns:
+        list[list[bool]]: tabella (lista di liste) di N righe ed N colonne. Ciascuna cella contiene un valore booleano
+        True (c'è una bomba) o False (nessuna bomba)
+    """
     # creo tabella NxN di False
     tab = []
     for i in range(N):
-        tab.append( [False]*N )
+        tab.append([False] * N)
 
         # riga = []
         # for j in range(N):
@@ -73,63 +84,86 @@ def crea_mine(N, M):
 
     # metto M mine nella tabella
     for m in range(M):
-        r = randint(0,N-1)
-        c = randint(0,N-1)
+        r = randint(0, N - 1)
+        c = randint(0, N - 1)
 
         while tab[r][c]:
-            r = randint(0,N-1)
-            c = randint(0,N-1)
+            r = randint(0, N - 1)
+            c = randint(0, N - 1)
 
         tab[r][c] = True
-    
+
     return tab
 
+
 def calcola_adiacenti(mine):
+    """Calcola, per ogni casella del campo minato, il numero di mine presenti nelle caselle adiacenti, e restituisce
+    una tabella di numeri, con tale conteggio per ogni casella.
+
+    Args:
+        mine (list[list[bool]]): campo minato (tabella NxN di booleani)
+
+    Returns:
+        list[list[int]]: tabella NxN contenente, per ogni casella [i][j], il numero di bombe nelle 8 caselle adiacenti
+    """
     N = len(mine)
-    num = [] # numeri di mine vicine
+    num = []  # numeri di mine vicine
 
     # costruisco un matrice di NxN zeri ()
     for i in range(N):
-        num.append([0]*N)
+        num.append([0] * N)
 
     for r in range(N):
         for c in range(N):
             num[r][c] = conta(mine, r, c)
     return num
 
+
 def conta(mine, r, c):
-    '''Determina quante mie ci sono nelle caselle ADIACENTI a mine[r][c].
-    Restituisce un intero tra 0 e 8'''
+    """Determina quante mie ci sono nelle caselle ADIACENTI a mine[r][c].
+    Restituisce un intero tra 0 e 8"""
     N = len(mine)
     tot = 0
 
     # for i in range (r-1, r+2):  # (righe che devo vedere)
-    for i in [r-1, r, r+1]:
-        for j in [c-1, c, c+1]:
+    for i in [r - 1, r, r + 1]:
+        for j in [c - 1, c, c + 1]:
             # if i e j sono nell'intervallo 0-N
             # if 0 <= i < N and 0 <= j < N and not(i==r and j==c):
             # if 0 <= i < N and 0 <= j < N and (i!=r or j!=c):
-            if 0 <= i < N and 0 <= j < N and (i,j)!=(r,c):
+            if 0 <= i < N and 0 <= j < N and (i, j) != (r, c):
                 if mine[i][j]:
                     tot = tot + 1
-
     return tot
 
+
 def print_mine(mine):
+    """Stampa il campo minato in modo esteticamente decente
+
+    Args:
+        mine (list[list[bool]]): il campo minato da stampare
+    """
     N = len(mine)
     for i in range(N):
         for j in range(N):
             if mine[i][j]:
-                print('x', end='')
+                print("x", end="")
             else:
-                print('.', end='')
+                print(".", end="")
         print()
 
-def print_vicini(vicini): 
+
+def print_vicini(vicini):
+    """Stampa i numeri di adiacenti (utile solo per debug/controllo)
+
+    Args:
+        vicini (list[list[int]]): tabella dei numeri di bombe adiacenti
+    """
     N = len(vicini)
     for i in range(N):
         for j in range(N):
-            print(vicini[i][j], end='')
+            print(vicini[i][j], end="")
         print()
 
-main() # chiamata della funzione principale
+
+main()  # chiamata della funzione principale
