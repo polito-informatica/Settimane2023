f7882c6723c0762615906a820c8984f3ed1b29c6
Fulvio Corno <fulvio.corno@polito.it>
2023-12-11 14:50:54 +0100

    esercizio da fare

---
+++ Settimana11/iscritti.py
@@ -0,0 +1,8 @@
+'''
+Dato l'elenco degli studenti iscritti, presente nel file '14BHDOA_2024_shuffled.csv,
+si vogliono creare molti file che contengano l'elenco "parziale" diviso per Corso di Studi.
+Ad esempio, il file "ELN1T3.csv" conterrà tutti e soli gli studenti che nel campo "CDS STUDENTE"
+hanno il valore "ELN1T3".
+Durante la creazione di tali file, si stampi anche il numero di studenti presenti
+in ciascuno dei gruppi.
+'''
